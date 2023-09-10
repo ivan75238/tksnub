7551ac20b7eb2aa23af9ca47973b91ae3610b66f
ifontosh <ifontosh@it-aces.com>
2023-09-10 19:32:49 +0800

    api php

---
+++ public/api.php
@@ -0,0 +1,9 @@
+<?php
+  if( $curl = curl_init() ) {
+    curl_setopt($curl, CURLOPT_URL, 'https://smsc.ru/sys/send.php?login=a.machnov@mail.ru&psw=6depnSs4L!G8FEC&phones='.$_POST['phones'].'&mes='.$_POST['mes']);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+    $out = curl_exec($curl);
+    echo $out;
+    curl_close($curl);
+  }
+?>
