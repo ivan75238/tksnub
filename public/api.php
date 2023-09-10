326a523a780d7dc506a4bc149e7676430c49f922
ifontosh <ifontosh@it-aces.com>
2023-09-10 19:46:18 +0800

    asda

--- public/api.php
+++ public/api.php
@@ -3,8 +3,8 @@
     curl_setopt($curl, CURLOPT_URL, 'https://smsc.ru/sys/send.php?login=a.machnov@mail.ru&psw=6depnSs4L!G8FEC&phones=79086587883&mes='.$_GET['mes']);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
     $out = curl_exec($curl);
-    $info = curl_getinfo($curl);
-    var_dump($info);
+    //$info = curl_getinfo($curl);
+    //var_dump($info);
     echo $out;
     curl_close($curl);
   }
