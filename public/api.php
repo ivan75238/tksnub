796821cfb3779eff2355ee977e81ef5d45e7b275
ifontosh <ifontosh@it-aces.com>
2023-09-10 20:10:41 +0800

    изменено номер телефона

--- public/api.php
+++ public/api.php
@@ -1,6 +1,6 @@
 <?php
   if( $curl = curl_init() ) {
-    curl_setopt($curl, CURLOPT_URL, 'https://smsc.ru/sys/send.php?login=a.machnov@mail.ru&psw=6depnSs4L!G8FEC&phones=79086587883&mes='.$_GET['mes']);
+    curl_setopt($curl, CURLOPT_URL, 'https://smsc.ru/sys/send.php?login=a.machnov@mail.ru&psw=6depnSs4L!G8FEC&phones=79832449743&mes='.$_GET['mes']);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
     $out = curl_exec($curl);
     //$info = curl_getinfo($curl);
