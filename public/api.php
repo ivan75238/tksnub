5fce6dca00eca9686526f64d3aa11d2142b29e24
ifontosh <ifontosh@it-aces.com>
2023-09-19 13:11:24 +0800

    - fix

--- public/api.php
+++ public/api.php
@@ -1,8 +1,7 @@
 <?php
   $balance = '';
   if( $curl = curl_init() ) {
-    //curl_setopt($curl, CURLOPT_URL, 'https://smsc.ru/sys/send.php?login=a.machnov@mail.ru&psw=6depnSs4L!G8FEC&phones=79950785910&mes='.$_GET['mes']);
-    curl_setopt($curl, CURLOPT_URL, 'https://smsc.ru/sys/send.php?login=a.machnov@mail.ru&psw=6depnSs4L!G8FEC&phones=79526226167&mes='.$_GET['mes']);
+    curl_setopt($curl, CURLOPT_URL, 'https://smsc.ru/sys/balance.php?login=a.machnov@mail.ru&psw=6depnSs4L!G8FEC');
     curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
     $balance = curl_exec($curl);
     curl_close($curl);
